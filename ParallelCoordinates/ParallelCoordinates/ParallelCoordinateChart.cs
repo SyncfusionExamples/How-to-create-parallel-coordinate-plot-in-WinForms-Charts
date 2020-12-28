e1c6bb014580cf8f79da7d05ab87d64ae403cc33
Devakumar Dhanapoosanam <devakumar.dhanapoosanam@syncfusion.com>
2020-12-28 13:04:36 +0530

    Added the parallel coordinate sample in github.

---
+++ ParallelCoordinates/ParallelCoordinates/ParallelCoordinateChart.cs
@@ -0,0 +1,127 @@
+﻿using Syncfusion.Windows.Forms.Chart;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+
+namespace ParallelCoordinates
+{
+    public class ParallelCoordinateChart : ChartControl
+    {
+        private BindingList<ChartModel> source;
+
+        public BindingList<ChartModel> DataSource
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+                GenerateSeries();
+            }
+        }
+
+        private ChartSeriesType type = ChartSeriesType.Line;
+
+        public ChartSeriesType SeriesType
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+                AddAxis();
+                GenerateSeries();
+            }
+        }
+
+        private List<CustomAxisModel> customAxisList;
+
+        public List<CustomAxisModel> CustomAxisCollection
+        {
+            get
+            {
+                return customAxisList;
+            }
+            set
+            {
+                customAxisList = value;
+                AddAxis();
+            }
+        }
+
+        private void AddAxis()
+        {
+            if (CustomAxisCollection != null)
+            {
+                foreach (var item in CustomAxisCollection)
+                {
+                    if (!Axes.Contains(item.CustomAxis))
+                        Axes.Add(item.CustomAxis);
+                }
+            }
+        }
+
+        public ParallelCoordinateChart() : base()
+        {
+            this.PrimaryXAxis.ValueType = ChartValueType.Category;
+            this.PrimaryXAxis.IsVisible = true;
+            this.PrimaryXAxis.OpposedPosition = true;
+            this.PrimaryXAxis.DrawGrid = false;
+            this.PrimaryYAxis.ValueType = ChartValueType.Double;
+            this.PrimaryYAxis.Range = new MinMaxInfo(0, 100, 10);
+            this.PrimaryYAxis.IsVisible = false;
+            this.PrimaryYAxis.DrawGrid = false;
+            this.PrimaryYAxis.Crossing = 0;
+            this.PrimaryYAxis.LineType.ForeColor = Color.Transparent;
+            AddAxis();
+            GenerateSeries();
+        }
+
+        private void GenerateSeries()
+        {
+            if (DataSource != null && CustomAxisCollection != null && CustomAxisCollection.Count >= DataSource.Count)
+            {
+                Series.Clear();
+
+                foreach (var item in DataSource)
+                {
+                    BindingList<SeriesModel> itemsSoruce = new BindingList<SeriesModel>();
+
+                    foreach (var value in item.Variable)
+                    {
+                        var index = item.Variable.IndexOf(value);
+                        var range = CustomAxisCollection[index].PlotRange;
+                        var diff = range.Max - range.Min;
+                        double result;
+                        double yvalue = 0;
+
+                        if (double.TryParse(value.ToString(), out result))
+                            yvalue = ((result - range.Min) / (double)diff) * 100;
+                        else
+                        {
+                            result = CustomAxisCollection[index].CustomAxisLabels.IndexOf(value.ToString());
+                            yvalue = ((result - range.Min) / (double)diff) * 100;
+                        }
+
+                        itemsSoruce.Add(new SeriesModel(CustomAxisCollection[index].AxisName, yvalue));
+                    }
+
+                    CategoryAxisDataBindModel dataSeriesModel = new CategoryAxisDataBindModel(itemsSoruce);
+                    dataSeriesModel.CategoryName = "XValues";
+                    dataSeriesModel.YNames = new string[] { "YValues" };
+                    ChartSeries LineSeries = new ChartSeries();
+                    LineSeries.Type = SeriesType;
+                    LineSeries.SortPoints = false;
+                    LineSeries.CategoryModel = dataSeriesModel;
+                    this.Series.Add(LineSeries);
+                }
+            }
+        }
+    }
+}
---
+++ ParallelCoordinates/ParallelCoordinates/SeriesModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelCoordinates
+{
+    public class SeriesModel
+    {
+        private string xValue;
+        private double yValue;
+
+        public string XValues
+        {
+            get
+            {
+                return xValue;
+            }
+            set
+            {
+                xValue = value;
+            }
+        }
+
+        public double YValues
+        {
+            get
+            {
+                return yValue;
+            }
+            set
+            {
+                yValue = value;
+            }
+        }
+
+        public SeriesModel(string x, double y)
+        {
+            XValues = x;
+            YValues = y;
+        }
+    }
+
+    public class ChartModel
+    {
+        private List<object> variables;
+
+        public List<object> Variable
+        {
+            get
+            {
+                return variables;
+            }
+            set
+            {
+                variables = value;
+            }
+        }
+
+        public ChartModel(List<object> values)
+        {
+            variables = values;
+        }
+    }
+}
